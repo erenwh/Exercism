82284dcdbb43fd5606fb157b595cfa17db0366ef
HanWang <erenwh@gmail.com>
2018-07-24 23:34:08 -0400

    better solution for isogram

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,7 +1,10 @@
 def is_isogram(string):
-    my_dict = {}
+    """my_dict = {}
     for c in string:
         my_dict[c.lower()] = my_dict.get(c.lower(), 0) + 1
     not_isogram = {k: v for (k, v) in my_dict.items()
                    if v > 1 and k.isalpha()}
     return not_isogram == {}
+    """
+    string = list(filter(str.isalpha, string.lower()))
+    return len(set(string)) == len(string)
