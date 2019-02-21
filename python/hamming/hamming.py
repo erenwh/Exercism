ef5daf58d4f5ea8259c653ba09d2d590da25d2da
HanWang <erenwh@gmail.com>
2019-02-20 23:30:30 -0500

    finished hamming, starting two-fer

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,4 +1,9 @@
 def distance(strand_a, strand_b):
-    if len(strand_a) == len(strand_b):
-        return False
-    return True
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Length Error")
+    else:
+        count = 0
+        for a, b in zip(strand_a, strand_b):
+            if a is not b:
+                count += 1
+    return count
